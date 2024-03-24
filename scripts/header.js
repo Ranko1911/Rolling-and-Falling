c5aa2da86872ff215602c5b3beea759a8dda0beb
Ancor <64937197+Ranko1911@users.noreply.github.com>
2024-03-24 20:10:12 +0000

    CODE - locally version

--- scripts/header.js
+++ scripts/header.js
@@ -1,31 +1,27 @@
 class SpecialHeader extends HTMLElement {
-  conectedCallback() {
-//     this.innerHTML = `
-//         <nav class="navbar navbar-expand-lg bg-black" data-bs-theme="dark">
-//     <div class="container-fluid p-0">
-//       <a class="navbar-brand ps-4" href="./index.html">
-//         <img src="./images/icons/dado_blanco_fondo_negro.webp" width="100" height="100"
-//           alt="Rolling and Falling Logo" /></a>
-//       <button class="navbar-toggler me-4" type="button" data-bs-toggle="collapse" data-bs-target="#headerNavBar"
-//         aria-controls="headerNavBar" aria-expanded="false" aria-label="Toggle navigation">
-//         <span class="navbar-toggler-icon"></span>
-//       </button>
-//       <div class="collapse navbar-collapse" id="headerNavBar">
-//         <div class="navbar-nav">
-//           <a id="header-home" class="nav-link" href="./index.html">Home</a>
-//           <a id="header-items" class="nav-link" href="./pages/items/test_display_item.html">Items</a>
-//           <a id="header-places" class="nav-link" href="./pages/places/test_place.html">Places</a>
-//           <a id="header-npcs" class="nav-link" href="./pages/NPCS/test_char.html">NPCs</a>
-//           <a id="header-events" class="nav-link" href="./pages/events/test_events.html">Events</a>
-//         </div>
-//       </div>
-//     </div>
-//     </div>
-//   </nav>
-//         `;
-        this.innerHTML = `
-            <p>Header</p>
-        `;
+  connectedCallback() {
+    this.innerHTML = `
+    <nav class="navbar navbar-expand-lg bg-black" data-bs-theme="dark">
+    <div class="container-fluid p-0">
+      <a class="navbar-brand ps-4" href="./index.html">
+        <img src="/images/icons/dado_blanco_fondo_negro.webp" width="100" height="100"
+          alt="Rolling and Falling Logo" /></a>
+      <button class="navbar-toggler me-4" type="button" data-bs-toggle="collapse" data-bs-target="#headerNavBar"
+        aria-controls="headerNavBar" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="headerNavBar">
+        <div class="navbar-nav">
+          <a id="header-home" class="nav-link" href="/index.html">Home</a>
+          <a id="header-items" class="nav-link" href="/pages/items/test_display_item.html">Items</a>
+          <a id="header-places" class="nav-link" href="/pages/places/test_place.html">Places</a>
+          <a id="header-npcs" class="nav-link" href="/pages/NPCS/test_char.html">NPCs</a>
+          <a id="header-events" class="nav-link" href="/pages/events/test_events.html">Events</a>
+        </div>
+      </div>
+    </div>
+  </nav>
+        `
   }
 }
 
