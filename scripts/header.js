c62bcadcc55b85cbaa3a74e5c1b24665ad288bcf
Ancor <64937197+Ranko1911@users.noreply.github.com>
2024-03-24 23:08:55 +0000

    locally source

--- scripts/header.js
+++ scripts/header.js
@@ -3,7 +3,7 @@ class SpecialHeader extends HTMLElement {
     this.innerHTML = `
     <nav class="navbar navbar-expand-lg bg-black" data-bs-theme="dark">
     <div class="container-fluid p-0">
-      <a class="navbar-brand ps-4" href="./index.html">
+      <a class="navbar-brand ps-4" href="/index.html">
         <img src="./images/icons/dado_blanco_fondo_negro.webp" width="100" height="100"
           alt="Rolling and Falling Logo" /></a>
       <button class="navbar-toggler me-4" type="button" data-bs-toggle="collapse" data-bs-target="#headerNavBar"
@@ -14,9 +14,9 @@ class SpecialHeader extends HTMLElement {
         <div class="navbar-nav">
           <a id="header-home" class="nav-link" href="./index.html">Home</a>
           <a id="header-items" class="nav-link" href="./pages/items/test_display_item.html">Items</a>
-          <a id="header-places" class="nav-link" href="./pages/places/test_display_events.html">Places</a>
-          <a id="header-charac" class="nav-link" href="./pages/charac/test_display_characters.html">Characters</a>
-          <a id="header-events" class="nav-link" href="./pages/events/test_display_events.html">Events</a>
+          <a id="header-places" class="nav-link" href="./pages/places/test_place.html">Places</a>
+          <a id="header-npcs" class="nav-link" href="./pages/NPCS/test_char.html">NPCs</a>
+          <a id="header-events" class="nav-link" href="./pages/events/test_events.html">Events</a>
         </div>
       </div>
     </div>
@@ -26,3 +26,5 @@ class SpecialHeader extends HTMLElement {
 }
 
 customElements.define("special-header", SpecialHeader);
+
+// alert(1);
