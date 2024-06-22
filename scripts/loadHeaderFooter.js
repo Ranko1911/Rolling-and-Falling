59a4367ef9eee2f1dc188d0e5ebf8e47173f61e5
Ancor <64937197+Ranko1911@users.noreply.github.com>
2024-06-22 20:47:44 +0000

    CODE - lost problem

--- scripts/loadHeaderFooter.js
+++ scripts/loadHeaderFooter.js
@@ -1,13 +1,13 @@
-document.addEventListener('DOMContentLoaded', function () {
-  fetch('/header.html')
-    .then((response) => response.text())
-    .then((data) => {
+document.addEventListener('DOMContentLoaded', function() {
+  fetch('/Rolling-and-Falling/header.html')
+    .then(response => response.text())
+    .then(data => {
       document.getElementById('header1').innerHTML = data;
     });
 
-  fetch('/footer.html')
-    .then((response) => response.text())
-    .then((data) => {
+  fetch('/Rolling-and-Falling/footer.html')
+    .then(response => response.text())
+    .then(data => {
       document.getElementById('footer1').innerHTML = data;
     });
 });
