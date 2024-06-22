a51b069a41dd7df73254f901563509c3aaf5b613
Ancor <64937197+Ranko1911@users.noreply.github.com>
2024-06-22 21:16:10 +0000

    FIX - display places v4

--- scripts/loadHeaderFooter.js
+++ scripts/loadHeaderFooter.js
@@ -1,11 +1,11 @@
 document.addEventListener('DOMContentLoaded', function() {
-  fetch('/header.html')
+  fetch('https://ranko1911.github.io/Rolling-and-Falling//header.html')
     .then(response => response.text())
     .then(data => {
       document.getElementById('header1').innerHTML = data;
     });
 
-  fetch('/footer.html')
+  fetch('https://ranko1911.github.io/Rolling-and-Falling//footer.html')
     .then(response => response.text())
     .then(data => {
       document.getElementById('footer1').innerHTML = data;
