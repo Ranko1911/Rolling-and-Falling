ed094ee485e721b081427886899ce9e772484287
Ancor <64937197+Ranko1911@users.noreply.github.com>
2024-06-22 16:27:41 +0000

    prettier installed

--- scripts/loadHeaderFooter.js
+++ scripts/loadHeaderFooter.js
@@ -1,13 +1,13 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
   fetch('/Rolling-and-Falling/header.html')
-    .then(response => response.text())
-    .then(data => {
+    .then((response) => response.text())
+    .then((data) => {
       document.getElementById('header1').innerHTML = data;
     });
 
   fetch('/Rolling-and-Falling/footer.html')
-    .then(response => response.text())
-    .then(data => {
+    .then((response) => response.text())
+    .then((data) => {
       document.getElementById('footer1').innerHTML = data;
     });
 });
