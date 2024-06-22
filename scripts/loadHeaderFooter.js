a77615972d8c8d8d69f5d96fa74592c66e46365e
Ancor <64937197+Ranko1911@users.noreply.github.com>
2024-06-22 20:49:01 +0000

    CODE - lost problem

--- scripts/loadHeaderFooter.js
+++ scripts/loadHeaderFooter.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
-  fetch('/Rolling-and-Falling/header.html')
+  fetch('/header.html')
     .then(response => response.text())
     .then(data => {
       document.getElementById('header1').innerHTML = data;
