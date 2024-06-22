39d26ac8b9168490c5924692367a286f55dfe771
Ancor <64937197+Ranko1911@users.noreply.github.com>
2024-06-22 21:36:32 +0000

    CODE - now it's the lost <p>

--- scripts/loadHeaderFooter.js
+++ scripts/loadHeaderFooter.js
@@ -1,13 +1,13 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
   fetch('https://ranko1911.github.io/Rolling-and-Falling//header.html')
-    .then(response => response.text())
-    .then(data => {
+    .then((response) => response.text())
+    .then((data) => {
       document.getElementById('header1').innerHTML = data;
     });
 
   fetch('https://ranko1911.github.io/Rolling-and-Falling//footer.html')
-    .then(response => response.text())
-    .then(data => {
+    .then((response) => response.text())
+    .then((data) => {
       document.getElementById('footer1').innerHTML = data;
     });
 });
